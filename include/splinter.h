d4b6ca7362b8f0bbbc319f12fd3eb733db4ac2f8
Bowie Owens <bowie.owens@csiro.au>
2022-07-11 13:37:09 +1000

    BUILD Added header that includes all other headers.

---
+++ include/splinter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "bspline.h"
+#include "bsplinebasis.h"
+#include "bsplinebasis1d.h"
+#include "bsplinebuilder.h"
+#include "datapoint.h"
+#include "datatable.h"
+#include "definitions.h"
+#include "function.h"
+#include "knots.h"
+#include "linearsolvers.h"
+#include "mykroneckerproduct.h"
+#include "saveable.h"
+#include "serializer.h"
+#include "utilities.h"
