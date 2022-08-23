72cba4ef0c22c282de81d3a8962675124bb1cdee
Bowie Owens <bowie.owens@csiro.au>
2022-08-23 11:41:28 +1000

    FIX `numVariables` still referenced in `assert()`.

--- include/bspline.h
+++ include/bspline.h
@@ -50,7 +50,7 @@ public:
 
     template <class x_type>
     void checkInput(x_type const& x) const {
-        assert(x.size() == numVariables);
+        assert(x.size() == getNumVariables());
     }
 
     template <class x_type>
